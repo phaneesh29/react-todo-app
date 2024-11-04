41b79092332eab432cfe39cf3d5d17e246f5c894
Sreephaneesha <kanugovisreephaneesha@gmail.com>
2024-11-04 12:04:04 +0530

    made premium

--- src/App.jsx
+++ src/App.jsx
@@ -18,7 +18,7 @@ const App = () => {
     toast.error("Invalid Input", {
       position: "top-right",
       autoClose: 3000,
-      theme: "dark"
+      theme: "colored"
     });
   };
 
@@ -26,7 +26,7 @@ const App = () => {
     toast.success("Success! Your Task was added.", {
       position: "top-right",
       autoClose: 3000,
-      theme: "dark",
+      theme: "colored",
     });
   };
 
@@ -92,17 +92,17 @@ const App = () => {
   return (
     <>
       <ToastContainer />
-      <div className='bg-slate-600 rounded-lg w-[500px] min-h-[600px] m-auto mt-10'>
-        <form onSubmit={(e) => handleFormSubmit(e)} className='border-b-4 border-b-orange-400 flex flex-col gap-4 p-7 bg-emerald-800 justify-center rounded-lg'>
-          <input type="text" name="task" value={title} onChange={(e) => handleTitle(e)} className='p-3 rounded-lg bg-transparent ring-4 ring-orange-800 focus:outline-none  text-2xl placeholder:text-2xl placeholder:text-gray-400' placeholder='Enter Task...' />
-          <textarea name="desc" value={desc} onChange={(e) => handleDesc(e)} className='bg-transparent ring-4 ring-orange-700 rounded-lg  p-3 min-h-[100px] resize-y max-h-[200px] focus:outline-none text-2xl placeholder:text-2xl placeholder:text-gray-400' placeholder='Enter Description....'></textarea>
-          <select name="category" value={selectedOption} onChange={handleSelect} className='p-3 rounded-lg bg-transparent text-2xl ring-4 ring-blue-400 focus:outline-none '>
+      <div className='bg-red-200 rounded-lg w-[500px] min-h-[600px] m-auto mt-10 ring-4 ring-emerald-600 shadow-lg'>
+        <form onSubmit={(e) => handleFormSubmit(e)} className='border-b-4 border-b-orange-400 flex flex-col gap-4 p-7 bg-gray-300 justify-center rounded-lg'>
+          <input type="text" name="task" value={title} onChange={(e) => handleTitle(e)} className='p-3 rounded-lg bg-transparent ring-2 ring-orange-800 focus:outline-none  text-2xl placeholder:text-2xl placeholder:text-gray-900' placeholder='Enter Task...' />
+          <textarea name="desc" value={desc} onChange={(e) => handleDesc(e)} className='bg-transparent ring-2 ring-orange-700 rounded-lg  p-3 min-h-[100px] resize-y max-h-[200px] focus:outline-none text-2xl placeholder:text-2xl placeholder:text-gray-900' placeholder='Enter Description....'></textarea>
+          <select name="category" value={selectedOption} onChange={handleSelect} className='p-3 rounded-lg bg-transparent text-2xl ring-2 ring-blue-700 focus:outline-none '>
             <option className='text-black' value="" disabled>Select an Option</option>
             <option className='text-black' value="gaming">Gaming</option>
             <option className='text-black' value="cooking">Cooking</option>
             <option className='text-black' value="studies">Studies</option>
           </select>
-          <button type="submit" className='p-3 bg-emerald-400 text-black rounded-lg text-xl font-semibold w-[300px] m-auto hover:bg-emerald-500 active:scale-75 transition-all'>Add Task</button>
+          <button type="submit" className='p-3 bg-teal-400 text-black rounded-lg ring-1 ring-blue-600 text-xl font-semibold w-[300px] m-auto hover:bg-teal-500 active:scale-75 transition-all'>Add Task</button>
         </form>
         <select name="category" value={categoryChange} onChange={handleCategoryChange} className='p-2 m-3 w-[200px] rounded-lg bg-transparent text-xl ring-4 ring-blue-400 focus:outline-none '>
           <option className='text-black' value="">Show All</option>
@@ -110,12 +110,12 @@ const App = () => {
           <option className='text-black' value="cooking">Cooking</option>
           <option className='text-black' value="studies">Studies</option>
         </select>
-        <ul className='rounded-lg p-3 bg-stone-900 flex flex-col gap-4 text-xl mb-7'>
+        <ul className='rounded-lg p-3 flex flex-col gap-4 text-xl mb-7'>
           {
             (taskArr.filter((t) => (
               categoryChange ? t.category === categoryChange : true
             ))).map((t, index) => (
-              <li key={index} className={` p-2 px-4 text-xl rounded-lg flex items-center justify-between ${t.isCompleted ? "bg-green-800 line-through" : "bg-blue-950"} `}>
+              <li key={index} className={` p-2 px-4 text-xl rounded-lg flex items-center justify-between ${t.isCompleted ? "bg-green-300 line-through" : "bg-blue-300"} `}>
                 <div className='flex flex-col justify-center rounded-lg'>
                   <span className='flex gap-4'>Title: <p>{t.title}</p></span>
                   <span className='flex gap-4'>Description: <p>{t.desc}</p></span>
